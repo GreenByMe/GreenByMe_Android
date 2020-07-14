7c167d6a16c43a74f6a4e9abdb4b1cec18000809
unknown <gkgkgkrj@gmail.com>
2020-07-14 21:45:29 +0900

    make project

---
+++ app/src/main/java/org/greenbyme/angelhack/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.greenbyme.angelhack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
