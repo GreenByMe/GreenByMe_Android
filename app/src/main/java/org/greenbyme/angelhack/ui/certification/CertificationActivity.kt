f59b5b1a57a5a25d2dfa4064e441bc4c72aa960f
unknown <gkgkgkrj@gmail.com>
2020-07-18 14:31:42 +0900

    conflict solved

--- app/src/main/java/org/greenbyme/angelhack/ui/certification/CertificationActivity.kt
+++ app/src/main/java/org/greenbyme/angelhack/ui/certification/CertificationActivity.kt
@@ -6,7 +6,6 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_certification.*
 import org.greenbyme.angelhack.R
 
